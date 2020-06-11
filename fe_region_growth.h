8c67b3dbcb0f45280f9256a18ec63aa9b75c760b
Shaun Zacharia <shaunzacharia5@gmail.com>
2020-06-11 20:00:09 +0530

    Create fe_region_growth.h
    CREATE - fe_region_growth.h header file for all constants and including libraries

---
+++ fe_region_growth.h
@@ -0,0 +1,12 @@
+#include<iostream>
+#include <chrono> 
+using namespace std;
+using namespace std::chrono;
+
+#define THRESHOLD 10
+#define STAR_MIN_PIXEL 3
+#define STAR_MAX_PIXEL 150
+#define MAX_STARS 100
+#define SKIP_PIXELS 2
+#define LENGTH 1280
+#define BREADTH 1024
